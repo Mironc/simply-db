b9cd97dcc324496fcae681f7ddf897f3605fb1cf
Mironc <mironc.dev@gmail.com>
2026-07-07 03:14:31 +0500

    chore(bench): add throughput metric to all benchmarks

--- benches/0_bench_cmp.rs
+++ benches/0_bench_cmp.rs
@@ -14,7 +14,14 @@ fn criterion_benchmark(c: &mut Criterion) {
             black_box(tokenvalue_cmp_not_str(&value));
         })
     });
-    let value = TokenValue::Sign(Sign::Plus);
+
+    c.bench_function("as_str_cmp_not_str", |b| {
+        b.iter(|| {
+            black_box(as_str_cmp_not_str(&value));
+        })
+    });
+
+    let value = TokenValue::Keyword("WHERE");
     c.bench_function("to_string_cmp_str", |b| {
         b.iter(|| black_box(to_string_cmp_str(&value)))
     });
@@ -24,18 +31,34 @@ fn criterion_benchmark(c: &mut Criterion) {
             black_box(tokenvalue_cmp_str(&value));
         })
     });
+
+    c.bench_function("as_str_cmp_str", |b| {
+        b.iter(|| {
+            black_box(as_str_cmp_str(&value));
+        })
+    });
 }
+
 fn to_string_cmp_not_str(value: &TokenValue) -> bool {
     value.to_string() == "+"
-}
-fn tokenvalue_cmp_not_str(value: &TokenValue) -> bool {
-    value == &TokenValue::Sign(Sign::Plus)
 }
 fn to_string_cmp_str(value: &TokenValue) -> bool {
     value.to_string() == "WHERE"
 }
-fn tokenvalue_cmp_str(value: &TokenValue) -> bool {
-    value == &TokenValue::Ident("WHERE".into())
+
+fn as_str_cmp_not_str(value: &TokenValue) -> bool {
+    value.as_str() == "+"
 }
+fn as_str_cmp_str(value: &TokenValue) -> bool {
+    value.as_str() == "WHERE"
+}
+
+fn tokenvalue_cmp_not_str(value: &TokenValue) -> bool {
+    value == &TokenValue::Sign(Sign::Plus)
+}
+fn tokenvalue_cmp_str(value: &TokenValue) -> bool {
+    value == &TokenValue::Ident("WHERE")
+}
+
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
---
+++ benches/3_query_parsing.rs
@@ -0,0 +1,45 @@
+use criterion::{Criterion, Throughput, criterion_group, criterion_main};
+use simply_db::sql::parser::query::parse_query;
+use simply_db::sql::parser::tokenizer::tokenize;
+use std::hint::black_box;
+
+// Helper macro to query_parse a string cleanly in tests
+macro_rules! query_parse {
+    ($input:expr) => {{
+        let tokens = tokenize($input);
+        parse_query(tokens)
+    }};
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Query parsing");
+
+    let query = "SELECT * FROM sometablename WHERE id == 0 AND name == 'Steve'";
+    group.throughput(Throughput::Bytes(query.len() as u64));
+    group.bench_function("select", |b| {
+        b.iter(|| {
+            black_box(query_parse!(query)).unwrap();
+        })
+    });
+
+    let query = "INSERT INTO sometablename (id, name) VALUES (0,'Steve')";
+    group.throughput(Throughput::Bytes(query.len() as u64));
+    group.bench_function("insert", |b| {
+        b.iter(|| {
+            black_box(query_parse!(query)).unwrap();
+        })
+    });
+
+    let query = "CREATE TABLE IF NOT EXISTS sometablename (id INT PRIMARY KEY, name TEXT NOT NULL)";
+    group.throughput(Throughput::Bytes(query.len() as u64));
+    group.bench_function("create table", |b| {
+        b.iter(|| {
+            black_box(query_parse!(query)).unwrap();
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
