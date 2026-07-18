7ef2d76dfbefc08d5f693a4a86922034b6328d4e
Mironc <mironc.dev@gmail.com>
2026-07-18 20:27:32 +0500

    fix(table): unique constraint check
    - check value for uniqueness excluding row that is being inserted.

--- storage/src/table.rs
+++ storage/src/table.rs
@@ -34,7 +34,7 @@ impl Table {
     pub fn insert_rows(&self, rows: Vec<Row>) -> Result<(), TableInsertError> {
         let mut validated_rows = Vec::new();
         for (i, row) in rows.iter().enumerate() {
-            self.validate_row(&rows[i..rows.len()], row)?;
+            self.validate_row(&rows[i + 1..rows.len()], row)?;
             validated_rows.push(row.clone());
         }
         for row in validated_rows.into_iter() {
@@ -102,8 +102,9 @@ mod tests {
         scalar, scalar_type,
         table::{Table, TableInsertError},
     };
+
     #[test]
-    fn insert_unique_constraint() {
+    fn insert_unique_constraint_error() {
         let field_type = FieldType::new(scalar_type!(Int), vec![FieldModifier::Unique]);
         let schema = Schema::new(vec![("id".to_owned(), field_type)]);
         let table = Table::new(schema);
@@ -113,8 +114,9 @@ mod tests {
         let res = table.insert_row(Row::new(SchemaValue::new(value)));
         assert_eq!(res, Err(TableInsertError::UniqueConstraint))
     }
+
     #[test]
-    fn insert_multiple_unique_constraint() {
+    fn insert_multiple_unique_constraint_error() {
         let field_type = FieldType::new(scalar_type!(Int), vec![FieldModifier::Unique]);
         let schema = Schema::new(vec![("id".to_owned(), field_type)]);
         let table = Table::new(schema);
@@ -127,8 +129,48 @@ mod tests {
         let res = table.insert_rows(rows);
         assert_eq!(res, Err(TableInsertError::UniqueConstraint))
     }
+
     #[test]
-    fn insert_field_mismatch() {
+    fn insert_multiple_unique_single_row_success() {
+        let field_type = FieldType::new(scalar_type!(Int), vec![FieldModifier::Unique]);
+        let schema = Schema::new(vec![("id".to_owned(), field_type)]);
+        let table = Table::new(schema);
+        let value = hashmap!("id".to_owned() => scalar!(Int(0)));
+        let rows = vec![Row::new(SchemaValue::new(value))];
+        let res = table.insert_rows(rows);
+        assert_eq!(res, Ok(()))
+    }
+
+    #[test]
+    fn insert_multiple_unique_success() {
+        let field_type = FieldType::new(scalar_type!(Int), vec![FieldModifier::Unique]);
+        let schema = Schema::new(vec![("id".to_owned(), field_type)]);
+        let table = Table::new(schema);
+        let value = hashmap!("id".to_owned() => scalar!(Int(0)));
+        let value1 = hashmap!("id".to_owned() => scalar!(Int(1)));
+        let rows = vec![
+            Row::new(SchemaValue::new(value)),
+            Row::new(SchemaValue::new(value1)),
+        ];
+        let res = table.insert_rows(rows);
+        assert_eq!(res, Ok(()))
+    }
+
+    #[test]
+    fn insert_single_unique_multiple_success() {
+        let field_type = FieldType::new(scalar_type!(Int), vec![FieldModifier::Unique]);
+        let schema = Schema::new(vec![("id".to_owned(), field_type)]);
+        let table = Table::new(schema);
+        let value = hashmap!("id".to_owned() => scalar!(Int(0)));
+        let value1 = hashmap!("id".to_owned() => scalar!(Int(1)));
+        let res = table.insert_row(Row::new(SchemaValue::new(value)));
+        let res1 = table.insert_row(Row::new(SchemaValue::new(value1)));
+        assert_eq!(res, Ok(()));
+        assert_eq!(res1, Ok(()));
+    }
+
+    #[test]
+    fn insert_field_mismatch_error() {
         let field_type = FieldType::new(scalar_type!(Int), vec![FieldModifier::NotNull]);
         let schema = Schema::new(vec![("id".to_owned(), field_type)]);
         let table = Table::new(schema);
