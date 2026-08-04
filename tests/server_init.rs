b9af4110f8e8181e3f5fdc09d752704c2d1bb1f8
Mironc <mironc.dev@gmail.com>
2026-08-05 02:22:43 +0500

    test: remove scattered boilerplate and compact test structure

--- tests/server_init.rs
+++ tests/server_init.rs
@@ -1,16 +1,89 @@
+use net::{
+    objects::{Health, Overview, ParseErrorDTO, SqlQueryOutput},
+    requests::SqlQueryRequest,
+};
 use tokio::net::TcpListener;
 
-pub const BASE_IP: &'static str = "127.0.0.1";
+const BASE_IP: &'static str = "127.0.0.1";
 
-pub async fn start_server() -> (std::io::Result<std::process::Child>, u16) {
-    let exe_path = env!("CARGO_BIN_EXE_server");
-    let listener = TcpListener::bind(format!("{}:0", BASE_IP)).await.unwrap(); //
-    let assigned_port = listener.local_addr().unwrap().port();
-    (
-        std::process::Command::new(exe_path)
+pub struct ServerInstance {
+    process: std::process::Child,
+    listen_ip: String,
+}
+
+impl ServerInstance {
+    pub async fn new() -> std::io::Result<Self> {
+        let listener = TcpListener::bind(format!("{}:0", BASE_IP)).await.unwrap(); //
+        let assigned_port = listener.local_addr().unwrap().port();
+        let listen_ip = format!("{}:{}", BASE_IP, assigned_port);
+        let process = std::process::Command::new("cargo")
+            .args(["run", "--bin", "server", "--all-features"])
+            .arg("--")
             .arg("--listen-ip")
-            .arg(format!("{}:{}", BASE_IP, assigned_port))
-            .spawn(),
-        assigned_port,
-    )
+            .arg(&listen_ip)
+            .spawn()?;
+        Ok(Self { process, listen_ip })
+    }
+
+    pub fn listen_ip(&self) -> &str {
+        &self.listen_ip
+    }
+
+    pub async fn send_ping(&self) -> String {
+        let res = reqwest::Client::new()
+            .get(format!("http://{}/ping", self.listen_ip()))
+            .send()
+            .await
+            .expect("Error while sending request");
+        res.text().await.expect("Error while parsing output")
+    }
+
+    pub async fn send_health(&self) -> Health {
+        let res = reqwest::Client::new()
+            .get(format!("http://{}/health", self.listen_ip()))
+            .send()
+            .await
+            .expect("Error while sending request");
+        res.json::<Health>()
+            .await
+            .expect("Error while parsing output")
+    }
+
+    pub async fn send_overview(url: &str) -> Overview {
+        let res = reqwest::Client::new()
+            .get(format!("http://{}/v1/overview", url))
+            .send()
+            .await
+            .expect("Error while sending request");
+        res.json::<Overview>()
+            .await
+            .expect("Error while parsing output")
+    }
+
+    pub async fn send_query(&self, query: &str) -> Result<SqlQueryOutput, ParseErrorDTO> {
+        let res = reqwest::Client::new()
+            .post(format!("http://{}/v1/query", self.listen_ip()))
+            .json(&SqlQueryRequest::new(query.to_owned()))
+            .send()
+            .await
+            .expect("Error while sending request");
+        println!("{}", res.status());
+        if res.status().is_success() {
+            Ok(res
+                .json::<SqlQueryOutput>()
+                .await
+                .expect("Error while parsing output"))
+        } else {
+            Err(res
+                .json::<ParseErrorDTO>()
+                .await
+                .expect("Error while parsing output"))
+        }
+    }
+}
+
+impl Drop for ServerInstance {
+    fn drop(&mut self) {
+        self.process.kill().expect("Couldn't kill server")
+    }
 }
--- tests/test_simple.rs
+++ tests/test_simple.rs
@@ -1,47 +1,35 @@
-mod requests;
 mod server_init;
 #[cfg(test)]
 mod tests {
     use net::objects::DatabaseState;
     use storage::scalar;
 
-    use crate::{
-        requests::{send_health, send_ping, send_query},
-        server_init::{BASE_IP, start_server},
-    };
+    use crate::server_init::ServerInstance;
 
     #[tokio::test]
     async fn test_setup() {
-        let (server_app, server_port) = start_server().await;
-        let mut server_app = server_app.expect("Couldn't start server");
-        let server_listen_ip = format!("{}:{}", BASE_IP, server_port);
-        let ping_text = send_ping(&server_listen_ip).await;
+        let instance = ServerInstance::new().await.expect("Couldn't start server");
+
+        let ping_text = instance.send_ping().await;
         assert_eq!(ping_text, "pong");
 
-        let health = send_health(&server_listen_ip).await;
+        let health = instance.send_health().await;
         assert_eq!(health.state(), DatabaseState::Healthy);
-
-        server_app.kill().expect("Couldn't kill process")
     }
     #[tokio::test]
     async fn basic_queries() {
-        let (server_app, server_port) = start_server().await;
-        let mut server_app = server_app.expect("Couldn't start server");
-        let server_listen_ip = format!("{}:{}", BASE_IP, server_port);
-        send_query(&server_listen_ip, "CREATE TABLE users (id INT, name TEXT)")
+        let instance = ServerInstance::new().await.expect("Couldn't start server");
+        instance
+            .send_query("CREATE TABLE users (id INT, name TEXT)")
             .await
             .unwrap();
 
-        send_query(
-            &server_listen_ip,
-            "INSERT INTO users (id, name) VALUES (0, 'Steve'), (1,'Alice')",
-        )
-        .await
-        .unwrap();
-
-        let output = send_query(&server_listen_ip, "SELECT * FROM users")
+        instance
+            .send_query("INSERT INTO users (id, name) VALUES (0, 'Steve'), (1,'Alice')")
             .await
             .unwrap();
+
+        let output = instance.send_query("SELECT * FROM users").await.unwrap();
         match output.output()[0].as_ref().unwrap() {
             query::QueryOutput::Rows(items) => {
                 assert_eq!(
@@ -54,7 +42,5 @@ mod tests {
             }
             query::QueryOutput::Nothing => panic!("Expected rows"),
         }
-
-        server_app.kill().expect("Couldn't kill process")
     }
 }
