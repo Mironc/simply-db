bca23266f69567fbecf5b749d610509dd7f8085c
Mironc <mironc.dev@gmail.com>
2026-08-08 22:12:07 +0500

    test(query): improve test quality and update test data
    - Improve naming for existing tests.
    - Add tests: `unique_constraint_error`, `expression_propagation_error`,
      `update_with_filter`, `update_with_unique`, `update_set_null`.

--- query/src/queries/update.rs
+++ query/src/queries/update.rs
@@ -162,8 +162,10 @@ impl UpdateQuery {
 }
 #[cfg(test)]
 mod test {
+    use std::ops::Deref;
+
     use storage::{
-        common_types::ScalarType,
+        common_types::{DataValue, ScalarType, ScalarValue},
         db::Database,
         row::Row,
         scalar,
@@ -172,7 +174,7 @@ mod test {
     use structures::VecMap;
 
     use crate::{
-        expr::{Expr, LiteralValue},
+        expr::{ArithmeticOp, ComparisonOp, Expr, ExprError, LiteralValue},
         queries::{
             create_table::CreateTable,
             insert::InsertQuery,
@@ -183,96 +185,290 @@ mod test {
     fn init_db() -> Database {
         let mut db = Database::new();
 
-        // Create a table with one field
         let mut field_types = VecMap::new();
         field_types.insert(
-            "age".to_string(),
-            FieldType::new(ScalarType::Int, vec![FieldModifier::NotNull]),
+            "id".to_string(),
+            FieldType::new(ScalarType::Int, vec![FieldModifier::PrimaryKey]),
+        );
+        field_types.insert(
+            "name".to_string(),
+            FieldType::new(ScalarType::Text, vec![FieldModifier::NotNull]),
+        );
+        field_types.insert(
+            "email".to_string(),
+            FieldType::new(ScalarType::Text, vec![]),
         );
         let schema = Schema::new(field_types);
 
         let create_table = CreateTable::new("table".to_string(), schema, false);
         create_table.execute(&mut db).unwrap();
 
-        let row = Row::new(vec![scalar!(Int(10))]);
-        let insert_table = InsertQuery::new("table".to_string(), vec!["age".to_owned()], vec![row]);
+        let row = Row::new(vec![
+            scalar!(Text("Alice")),
+            scalar!(Int(0)),
+            scalar!(Text("alice@email.com")),
+        ]);
+        let row1 = Row::new(vec![
+            scalar!(Text("Bob")),
+            scalar!(Int(1)),
+            scalar!(Text("bob@email.com")),
+        ]);
+        let insert_table = InsertQuery::new(
+            "table".to_string(),
+            vec!["name".to_owned(), "id".to_owned(), "email".to_owned()],
+            vec![row, row1],
+        );
         insert_table.execute(&mut db).unwrap();
         db
     }
+
     #[test]
-    fn unknown_table() {
+    fn unknown_table_error() {
         let mut db = Database::new();
 
-        let update = UpdateQuery::new(
-            "nonexistent_table".to_string(),
-            vec![("age".to_string(), Expr::Literal(LiteralValue::Int(10)))],
-            None,
-        );
+        let update = UpdateQuery::new("nonexistent_table".to_string(), vec![], None);
         assert!(update.execute(&mut db).is_err());
     }
 
     #[test]
-    fn missing_field() {
+    fn missing_field_error() {
         let mut db = init_db();
+        let update = UpdateQuery::new(
+            "table".to_string(),
+            vec![("field".to_string(), Expr::Literal(LiteralValue::Null))],
+            None,
+        );
+        assert_eq!(
+            update.execute(&mut db).unwrap_err(),
+            UpdateError::NoField {
+                field: "field".to_owned()
+            }
+        );
+    }
+
+    #[test]
+    fn unexpected_null_error() {
+        let mut db = init_db();
+
+        let update = UpdateQuery::new(
+            "table".to_string(),
+            vec![("name".to_string(), Expr::Literal(LiteralValue::Null))],
+            None,
+        );
+        assert_eq!(
+            update.execute(&mut db).unwrap_err(),
+            UpdateError::UnexpectedNull {
+                field: "name".to_owned()
+            }
+        );
+    }
+
+    #[test]
+    fn type_mismatch_error() {
+        let mut db = init_db();
+
+        let update = UpdateQuery::new(
+            "table".to_string(),
+            vec![(
+                "id".to_string(),
+                Expr::Literal(LiteralValue::Text("Alice".to_owned())),
+            )],
+            None,
+        );
+        assert_eq!(
+            update.execute(&mut db).unwrap_err(),
+            UpdateError::SetTypeMismatch {
+                expected: ScalarType::Int,
+                given: DataValue::Scalar(ScalarValue::Text("Alice".to_owned()))
+            }
+        );
+    }
+
+    #[test]
+    fn expression_propagation_error() {
+        let db = init_db();
+
         let update = UpdateQuery::new(
             "table".to_string(),
             vec![(
                 "name".to_string(),
-                Expr::Literal(LiteralValue::Text("Alice".to_owned())),
+                Expr::Arithmetic(Box::new(ArithmeticOp::Add(
+                    Expr::Field("name".to_string()),
+                    Expr::Literal(LiteralValue::Int(1)),
+                ))),
             )],
             None,
         );
-        assert!(update.execute(&mut db).is_err());
-    }
-
-    #[test]
-    fn null_value_on_non_nullable_field() {
-        let mut db = init_db();
+        assert_eq!(
+            update.execute(&db).unwrap_err(),
+            UpdateError::ExprErr(ExprError::NotApplicable)
+        );
 
         let update = UpdateQuery::new(
             "table".to_string(),
-            vec![("age".to_string(), Expr::Literal(LiteralValue::Null))],
-            None,
+            vec![(
+                "name".to_string(),
+                Expr::Literal(LiteralValue::Text("Steve".to_string())),
+            )],
+            Some(Expr::Comparison(Box::new(ComparisonOp::Eq(
+                Expr::Field("name".to_string()),
+                Expr::Literal(LiteralValue::Int(1)),
+            )))),
         );
-        assert!(update.execute(&mut db).is_err());
+        assert_eq!(
+            update.execute(&db).unwrap_err(),
+            UpdateError::ExprErr(ExprError::NotApplicable)
+        );
     }
-
     #[test]
-    fn type_mismatch() {
+    fn expression_returns_not_bool_error() {
         let mut db = init_db();
 
         let update = UpdateQuery::new(
             "table".to_string(),
             vec![(
-                "age".to_string(),
-                Expr::Literal(LiteralValue::Text("Alice".to_owned())),
+                "name".to_string(),
+                Expr::Literal(LiteralValue::Text("Steve".to_owned())),
+            )],
+            Some(Expr::Literal(LiteralValue::Int(10))),
+        );
+        assert_eq!(update.execute(&mut db).unwrap_err(), UpdateError::BadExpr);
+    }
+
+    #[test]
+    fn unique_constraint_error() {
+        let db = init_db();
+        let update = UpdateQuery::new(
+            "table".to_string(),
+            vec![("id".to_string(), Expr::Literal(LiteralValue::Int(0)))],
+            None,
+        );
+        assert_eq!(
+            update.execute(&db).unwrap_err(),
+            UpdateError::UniqueConstraint {
+                field: "id".to_owned()
+            }
+        );
+    }
+
+    #[test]
+    fn update() {
+        let mut db = init_db();
+
+        let update = UpdateQuery::new(
+            "table".to_string(),
+            vec![(
+                "name".to_string(),
+                Expr::Literal(LiteralValue::Text("Steve".to_owned())),
             )],
             None,
         );
-        assert!(update.execute(&mut db).is_err());
-    }
-
-    #[test]
-    fn bad_filter_expression() {
-        let mut db = init_db();
-
-        let update = UpdateQuery::new(
-            "table".to_string(),
-            vec![("age".to_string(), Expr::Literal(LiteralValue::Int(10)))],
-            Some(Expr::Literal(LiteralValue::Int(10))),
+        assert!(update.execute(&mut db).is_ok());
+        assert_eq!(
+            db.get_table("table").unwrap().rows().deref(),
+            &vec![
+                Row::new(vec![
+                    scalar!(Int(0)),
+                    scalar!(Text("Steve".to_string())),
+                    scalar!(Text("alice@email.com"))
+                ]),
+                Row::new(vec![
+                    scalar!(Int(1)),
+                    scalar!(Text("Steve".to_string())),
+                    scalar!(Text("bob@email.com".to_string()))
+                ])
+            ]
         );
-        assert_eq!(update.execute(&mut db), Err(UpdateError::BadExpr));
     }
 
     #[test]
-    fn success() {
+    fn update_with_filter() {
         let mut db = init_db();
 
         let update = UpdateQuery::new(
             "table".to_string(),
-            vec![("age".to_string(), Expr::Literal(LiteralValue::Int(30)))],
-            None,
+            vec![(
+                "name".to_string(),
+                Expr::Literal(LiteralValue::Text("Steve".to_owned())),
+            )],
+            Some(Expr::Comparison(Box::new(ComparisonOp::Eq(
+                Expr::Field("id".to_owned()),
+                Expr::Literal(LiteralValue::Int(0)),
+            )))),
         );
         assert!(update.execute(&mut db).is_ok());
+        assert_eq!(
+            db.get_table("table").unwrap().rows().deref(),
+            &vec![
+                Row::new(vec![
+                    scalar!(Int(0)),
+                    scalar!(Text("Steve".to_string())),
+                    scalar!(Text("alice@email.com"))
+                ]),
+                Row::new(vec![
+                    scalar!(Int(1)),
+                    scalar!(Text("Bob".to_string())),
+                    scalar!(Text("bob@email.com".to_string()))
+                ])
+            ]
+        );
+    }
+
+    #[test]
+    fn update_with_unique() {
+        let db = init_db();
+        let update = UpdateQuery::new(
+            "table".to_string(),
+            vec![(
+                "id".to_string(),
+                Expr::Arithmetic(Box::new(ArithmeticOp::Add(
+                    Expr::Field("id".to_owned()),
+                    Expr::Literal(LiteralValue::Int(2)),
+                ))),
+            )],
+            None,
+        );
+        assert!(update.execute(&db).is_ok());
+        assert_eq!(
+            db.get_table("table").unwrap().rows().deref(),
+            &vec![
+                Row::new(vec![
+                    scalar!(Int(2)),
+                    scalar!(Text("Alice".to_string())),
+                    scalar!(Text("alice@email.com"))
+                ]),
+                Row::new(vec![
+                    scalar!(Int(3)),
+                    scalar!(Text("Bob".to_string())),
+                    scalar!(Text("bob@email.com".to_string()))
+                ])
+            ]
+        );
+    }
+    #[test]
+    fn update_set_null() {
+        let mut db = init_db();
+
+        let update = UpdateQuery::new(
+            "table".to_string(),
+            vec![("email".to_string(), Expr::Literal(LiteralValue::Null))],
+            None,
+        );
+        update.execute(&mut db).unwrap();
+        assert_eq!(
+            db.get_table("table").unwrap().rows().deref(),
+            &vec![
+                Row::new(vec![
+                    scalar!(Int(0)),
+                    scalar!(Text("Alice")),
+                    DataValue::Null,
+                ]),
+                Row::new(vec![
+                    scalar!(Int(1)),
+                    scalar!(Text("Bob".to_string())),
+                    DataValue::Null,
+                ])
+            ]
+        );
     }
 }
