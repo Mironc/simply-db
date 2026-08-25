9c35d31bc1fd6b0fbed9bc9b78df6b5d08aa6e2c
Mironc <mironc.dev@gmail.com>
2026-08-26 00:58:48 +0500

    fix(api): expose modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-extern crate parser;
-extern crate query;
-extern crate storage;
+pub extern crate parser;
+pub extern crate query;
+pub extern crate storage;
 mod sql_executor;
 pub use sql_executor::{DatabaseContext, MetricsCollector};
